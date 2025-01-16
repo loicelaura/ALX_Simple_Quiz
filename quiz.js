8b5ac6ae3d10e82548333bffaca1c049e0ee3b43
loice okello <loicelaura@gmail.com>
2025-01-16 08:16:05 +0300

    initial commit

---
+++ quiz.js
@@ -0,0 +1,25 @@
+function checkAnswer () {
+    //Identify the Correct Answer
+    const correctAnswer ="4";
+
+     //Retrieve the User’s Answer
+    const selectedAnswer = document.querySelector('input[name="quiz"]:checked');
+
+    if (!selectedAnswer) {
+        document.getElementById("feedback").textContent = "Please select an answer before submitting.";
+        return;
+    }
+
+
+    const userAnswer = selectedAnswer.value
+  
+     //Compare the User’s Answer with the Correct Answer
+    if(userAnswer === correctAnswer){
+        document.getElementById("feedback").textContent = "Correct! well done.";
+    } else {
+        document.getElementById("feedback").textContent = "That's incorrect. Try again";
+    }
+}
+
+       //Add an Event Listener to the Submit Button
+       document.getElementById("submit-answer").addEventListener ("click", checkAnswer);
